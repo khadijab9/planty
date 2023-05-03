dbd3eb66dfc4783ae63b699025e4155a4dba8624
khadija bidari <bidari.khadja@hotmail.fr>
2023-05-04 00:10:40 +0200

    ajout de theme.css, footer.php et ajout ligne 8 sur function.php

--- wp-content/themes/planty/functions.php
+++ wp-content/themes/planty/functions.php
@@ -5,4 +5,5 @@ add_action('wp_enqueue_scripts', 'theme_enqueue_styles');
 function theme_enqueue_styles() {
        // Chargement du style.css du thème parent blankSl
        wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
-}
+       wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/css/theme.css', array(), filemtime(get_stylesheet_directory() . '/css/theme.css'));
+    }
