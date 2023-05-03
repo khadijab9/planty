8a9ca7bd03f004edfa8d02f25f19cf2747d1a9c6
khadija bidari <bidari.khadja@hotmail.fr>
2023-05-04 00:06:42 +0200

    ajout du header image du theme

--- wp-content/themes/planty/functions.php
+++ wp-content/themes/planty/functions.php
@@ -3,6 +3,6 @@
 // Action qui permet de charger des scripts dans notre thème
 add_action('wp_enqueue_scripts', 'theme_enqueue_styles');
 function theme_enqueue_styles() {
-       // Chargement du style.css du thème parent blankSlate
+       // Chargement du style.css du thème parent blankSl
        wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
 }
