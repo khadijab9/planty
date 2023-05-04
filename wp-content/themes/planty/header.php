16057fb1b8ab5e8425da364e3d26fb0e9b1fe146
khadija bidari <bidari.khadja@hotmail.fr>
2023-05-05 00:11:55 +0200

    modification header et css

--- wp-content/themes/planty/header.php
+++ wp-content/themes/planty/header.php
@@ -26,15 +26,32 @@ if ( is_front_page() || is_home() || is_front_page() && is_home() ) { echo '</h1
         </div>
         <nav class=navbar >
             <img class=logo src="/planty/wp-content/themes/planty/image/logo.png" alt="logo planty">
-            
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Nous rencontrer</a>
+                    </li>
+               <li class="nav-item dropdown">
+                    <a class="nav-link" href="#" id="navbarDropdown" role="button" data-toggle="admin" aria-haspopup="true" aria-expanded="false">
+                    Admin
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-linkLast" href="#">Commander</a>
+                </li>
+            </ul>
+
     <?php wp_nav_menu( array( 
         'theme_location' => 'main-menu', 
         'link_before' => '<span itemprop="name">', 
         'link_after' => '</span>',
         'menu_class' => 'navbar-nav mr-auto' ) ); ?>
-
  
         </nav>
+
+   
+            </div>
+</nav>
         
 
     </header>
