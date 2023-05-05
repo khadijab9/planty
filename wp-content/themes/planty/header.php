b7ab5d2a0fdf0d63caa07511b7cf7908ca8580ae
khadija bidari <bidari.khadja@hotmail.fr>
2023-05-05 11:54:58 +0200

    modification de header.php et css

--- wp-content/themes/planty/header.php
+++ wp-content/themes/planty/header.php
@@ -1,60 +1,28 @@
 <!DOCTYPE html>
-<html <?php language_attributes(); ?> <?php blankslate_schema_type(); ?>>
+<html <?php language_attributes(); ?>>
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
 <meta name="viewport" content="width=device-width" />
 <?php wp_head(); ?>
 </head>
+<body>
 
-<body <?php body_class(); ?>>
 
-<?php wp_body_open(); ?>
+<header id="header" role="banner">
 
-<div id="wrapper" class="hfeed">
-    <header id="header" role="banner">
-        <div id="branding">
-            <div id="site-title" itemprop="publisher" itemscope itemtype="https://schema.org/Organization">
-<?php
-if ( is_front_page() || is_home() || is_front_page() && is_home() ) { echo '<h1>'; }
-echo '<a href="' . esc_url( home_url( '/' ) ) . '" title="' . esc_attr( get_bloginfo( 'name' ) ) . '" rel="home" itemprop="url"><span itemprop="name">' . esc_html( get_bloginfo( 'name' ) ) . '</span></a>';
-if ( is_front_page() || is_home() || is_front_page() && is_home() ) { echo '</h1>'; }
-?>
 
-        </div>
 
-        <div id="site-description"<?php if ( !is_single() ) { echo ' itemprop="description"'; } ?>><?php bloginfo( 'description' ); ?></div>
-        </div>
-        <nav class=navbar >
-            <img class=logo src="/planty/wp-content/themes/planty/image/logo.png" alt="logo planty">
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav mr-auto">
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Nous rencontrer</a>
-                    </li>
-               <li class="nav-item dropdown">
-                    <a class="nav-link" href="#" id="navbarDropdown" role="button" data-toggle="admin" aria-haspopup="true" aria-expanded="false">
-                    Admin
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-linkLast" href="#">Commander</a>
-                </li>
-            </ul>
+<nav class=nav id="menu" role="navigation" itemscope itemtype="https://schema.org/SiteNavigationElement">
+<img id=logo src="wp-content/themes/planty/image/logo.png" alt="logo du site">
+<?php wp_nav_menu( array( 
+    'theme_location' => 'main-menu', 
+    'link_before' => '<span itemprop="name">', 
+    'link_after' => '</span>' ,
+    'menu_class' => 'navbar-nav mr-auto',
+    ) )
+    ; ?>
 
-    <?php wp_nav_menu( array( 
-        'theme_location' => 'main-menu', 
-        'link_before' => '<span itemprop="name">', 
-        'link_after' => '</span>',
-        'menu_class' => 'navbar-nav mr-auto' ) ); ?>
- 
-        </nav>
-
-   
-            </div>
 </nav>
-        
-
-    </header>
-
+</header>
 <div id="container">
 <main id="content" role="main">
