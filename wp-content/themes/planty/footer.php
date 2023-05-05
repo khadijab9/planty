92c9f2ee01e0fec64abc57c1318176730550af55
khadija bidari <bidari.khadja@hotmail.fr>
2023-05-05 16:45:52 +0200

    modifction footer et functions

--- wp-content/themes/planty/footer.php
+++ wp-content/themes/planty/footer.php
@@ -1,9 +1,13 @@
 </main>
-<?php get_sidebar(); ?>
+
 </div>
 <footer id="footer" role="contentinfo">
 <div id="copyright">
-&copy; <?php echo esc_html( date_i18n( __( 'Y', 'blankslate' ) ) ); ?> <?php echo esc_html( get_bloginfo( 'name' ) ); ?>
+<?php wp_nav_menu( array( 
+    'theme_location' => 'footer-menu',
+   
+    ) )
+    ; ?>
 </div>
 </footer>
 </div>
--- wp-content/themes/planty/functions.php
+++ wp-content/themes/planty/functions.php
@@ -7,3 +7,8 @@ function theme_enqueue_styles() {
        wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
        wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/css/theme.css', array(), filemtime(get_stylesheet_directory() . '/css/theme.css'));
     }
+
+function planty_supports(){
+   register_nav_menu('footer-menu', 'pieds de page');
+}
+
